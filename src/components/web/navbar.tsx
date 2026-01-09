a08508730d3df5ae52b83df3ed2063c7958a1afc
Marios Kanellopoulos <mariosknl@outlook.com>
2026-01-09 13:45:56 +0200

    Implement theme toggle functionality and update Navbar
    - Added ThemeToggle component for switching between light, dark, and system themes.
    - Introduced ThemeProvider to manage theme state and apply it across the application.
    - Updated Navbar to include ThemeToggle and replace the Sign Up button with Login and Get Started buttons.
    - Modified RootDocument to wrap children with ThemeProvider for theme context.

--- src/components/web/navbar.tsx
+++ src/components/web/navbar.tsx
@@ -1,6 +1,9 @@
+import { Button } from '../ui/button'
+import { ThemeToggle } from './them-toggle'
+
 export function Navbar() {
   return (
-    <nav className="sticky top-0 z-50 border-b bg-amber-50 backdrop-blur-2xl">
+    <nav className="sticky top-0 z-50 border-b bg-background/95 backdrop-blur supports-backdrop-filter:bg-background/60">
       <div className="mx-auto flex h-16 max-w-6xl items-center justify-between px-4">
         <div className="flex items-center gap-2">
           <img
@@ -12,9 +15,9 @@ export function Navbar() {
         </div>
 
         <div className="flex items-center gap-3">
-          <button className="text-white bg-red-500 px-3 py-1 rounded-lg">
-            Sign Up
-          </button>
+          <ThemeToggle />
+          <Button variant={'secondary'}>Login</Button>
+          <Button>Get Started</Button>
         </div>
       </div>
     </nav>
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,10 +1,14 @@
-import { createFileRoute } from "@tanstack/react-router";
-import { ComponentExample } from "@/components/component-example";
+import { createFileRoute } from '@tanstack/react-router'
+import { ComponentExample } from '@/components/component-example'
+import { Navbar } from '@/components/web/navbar'
 
-export const Route = createFileRoute("/")({ component: App });
+export const Route = createFileRoute('/')({ component: App })
 
 function App() {
-return (
-  <ComponentExample />
-);
+  return (
+    <div>
+      <Navbar />
+      <ComponentExample />
+    </div>
+  )
 }
